0807050bf32e56b6ecca4deab667bf055e57c56a
Borneel B. Phukan <borneelphukan@gmail.com>
2026-03-27 16:36:21 +0100

    cors fixes

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -7,12 +7,22 @@ import * as express from 'express';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: [
-      'http://localhost:3001',
-      'http://localhost:3000',
-      'https://admin.thelegacyapartment.co.in',
-      'https://thelegacyapartment.co.in'
-    ],
+    origin: (origin, callback) => {
+      const allowedOrigins = [
+        'http://localhost:3001',
+        'http://localhost:3000',
+        'https://admin.thelegacyapartment.co.in',
+        'https://thelegacyapartment.co.in',
+        'https://www.thelegacyapartment.co.in',
+        'https://thelegacyapartment.netlify.app',
+        'https://admin-thelegacyapartment.netlify.app'
+      ];
+      if (!origin || allowedOrigins.includes(origin) || origin.endsWith('.netlify.app')) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
